33b4f8a746dd33c141f515368c95fa4451b8394e
root <you@example.com>
2023-08-10 12:47:04 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,19 @@
+/*
+ * File: 4-puts.c
+ * Auth: Mouthafar (Elikor-dev) 10/8/2023
+ */
+
 #include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
+
 return (0);
 }
