4b7fda256b666802058fd48e5947270d53089c00
root <you@example.com>
2023-08-11 04:48:55 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,19 @@
 /*
  * File: 5-printf.c
- * Auth: Mouthafar (Elikor-dev) 11/8/2023
+ * Auth: Mouthafar (Elikor-dev)
  */
 
 #include <stdio.h>
 
 /**
- * main - Prints  prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
  *
  * Return: Always 0.
  */
 int main(void)
-{	
+{
 printf("with proper grammar, but the outcome is a piece of art,\n");
+
 return (0);
 }
